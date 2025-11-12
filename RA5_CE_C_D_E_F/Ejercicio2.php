0746959553163985aa75a88d1158b6e232cb103b
EnekoTH <etellaecheh2401@educantabria.es>
2025-11-13 00:09:43 +0100

    cambio en el ejercicio 2

--- RA5_CE_C_D_E_F/Ejercicio2.php
+++ RA5_CE_C_D_E_F/Ejercicio2.php
@@ -1,7 +1,8 @@
 <?php
-// Mostrar números primos entre 3 y 999
+$contador = 0;
 for ($num = 3; $num <= 999; $num++) {
     $esPrimo = true;
+
     for ($div = 2; $div <= sqrt($num); $div++) {
         if ($num % $div == 0) {
             $esPrimo = false;
@@ -10,6 +11,12 @@ for ($num = 3; $num <= 999; $num++) {
     }
     if ($esPrimo) {
         echo $num . " ";
+        $contador++;
+
+        // Cada 10 números, salto de línea
+        if ($contador % 10 == 0) {
+            echo "<br>";
+        }
     }
 }
 ?>
